cf2909171c36949186f902efc1d799b8eebac1d3
pbui3 <46579684+pbui3@users.noreply.github.com>
2019-01-15 16:44:24 -0700

    Completed coding for Die class

--- CSReviewSolution/OOPsReview/Program.cs
+++ CSReviewSolution/OOPsReview/Program.cs
@@ -10,8 +10,12 @@ namespace OOPsReview
     {
         static void Main(string[] args)
         {
-            Die blah = new Die();
-            Console.WriteLine(blah.MyProperty);
+            Die blah = new Die(6, "black", 4);
+            blah.FaceValue = 10;
+            Console.WriteLine(blah.FaceValue);
+            Console.ReadLine();
+            Die blob = new Die();
+            Console.WriteLine(blob.FaceValue);
             Console.ReadLine();
         }
     }
