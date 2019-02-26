5cba477f5290d53f3e0ffab620b376c1c3ec674d
pbui3 <46579684+pbui3@users.noreply.github.com>
2019-02-26 16:17:07 -0700

    Intro to ASPX

---
+++ BasicASPX/WebApp/SamplePages/FirstPage.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp.SamplePages
+{
+    public partial class FirstPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //This method will execute each and every time this page is processed
+            //This method will execute before any event method
+        }
+
+        protected void PressMe_Click(object sender, EventArgs e)
+        {
+            OutputMessage.Text = "Hello " + YourName.Text;
+        }
+    }
+}
