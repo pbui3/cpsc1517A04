e4522133ebeff076098985bea482efb7808ca0e0
pbui3 <46579684+pbui3@users.noreply.github.com>
2019-03-15 11:49:40 -0600

    setup CSNet DB

---
+++ CSNet/NorthwindSystem.Data/Product.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//The annotation used within the .Data project will require the System.ComponentModel.DataAnnotaions assembly
+//This assembly is added via your references
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace NorthwindSystem.Data
+{
+    //Use an annotation to link this class to the appropriate SQL table
+    [Table("Products")]
+    public class Product
+    {
+        //Mapping of the SQL table attributes will be to class prop
+
+            private string _QuantityPerUnit;
+        //Use an annotaion to identify the primary key
+        //1) Identity pkey on your SQL table
+        //   [Key] Pkey name must end in ID or Id
+        //2) A compound pkey on your SQL table
+        //   [Key, Column(Order=n)] Where n is the natural number indicating the physical order of the attribute in the pkey
+        //3) A user supplied pkey on your SQL table
+        //   [Key, DatabaseGenerated(DatabaseGeneratedOptions.None)]
+        [Key]
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int? SupplierID { get; set; }
+        public int? CategoryID { get; set; }
+        public string QuantityPerUnit
+        {
+            get
+            {
+                return _QuantityPerUnit;
+            }
+            set
+            {
+                _QuantityPerUnit = string.IsNullOrEmpty(value.Trim()) ? null : value;
+            }
+        }
+        public decimal? UnitPrice { get; set; }
+        public Int16? UnitsInStock { get; set; }
+        public Int16? UnitsOnOrder { get; set; }
+        public Int16? ReorderLevel { get; set; }
+        public bool Discontinued { get; set; }
+
+        //Sample of a computed field on SQL
+        //To annotate this property to be takebn as a SQL computed field, use
+        //[DatabaseGenerated(DatabaseGeneratedOptions.Computed)]
+        //public decimal Total { get; set; }
+
+        //Sample creating a read only property that is not an actual field on your SQL table (FirstName LastName)
+        //Use the NotMapped annotation to handle this
+        //[NotMapped]
+        //public string FullName
+        //{
+        //    get FirstName + " " + LastName;
+        //}
+    }
+}
---
+++ CSNet/NorthwindSystem/DAL/NorthwindContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//This class needs to have access to ADO.Net entity framework
+//The Nuget package EntityFramework has already been added
+//This project also need the assemby System.Data.Entity
+//This project will need using clauses that points to
+//a) the System.Data.Entity namespace
+//b) Your data project namesapce
+#region Additional Namespaces
+using System.Data.Entity;
+using NorthwindSystem.Data;
+#endregion
+
+namespace NorthwindSystem.DAL
+{
+    //Thi scontext class will need to inherit DbContext from entity framework
+    internal class NorthwindContext:DbContext
+    {
+        //Setup your class default constructor to supply connection string name to the DbContext inherited class
+        public NorthwindContext():base("NWDB")
+        {
+
+        }
+
+        //Create an EntityFramework DbSet<T> for each mapped SQL table
+        //<T> is your class in the .Data project
+        public DbSet<Product> Products { get; set; }
+    }
+}
